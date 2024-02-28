7c0ba0e985b26d79b32d0e0f0577bfa27279eb92
P1nkG00s3 <147384574+P1nkG00s3@users.noreply.github.com>
2024-02-28 07:59:40 +0300

    Add db

--- PycharmProjects/fridgeo_django/FridgeoBack/fridgeo/models.py
+++ PycharmProjects/fridgeo_django/FridgeoBack/fridgeo/models.py
@@ -1,3 +1,12 @@
+import email_validator
 from django.db import models
 
 # Create your models here.
+
+
+class User(models.Model):
+    name = models.CharField(max_length=30)
+    surname = models.CharField(max_length=30)
+    email = models.EmailField(null=False)
+    password = models.CharField(max_length=20, null=False)
+
--- PycharmProjects/fridgeo_django/FridgeoBack/fridgeo/urls.py
+++ PycharmProjects/fridgeo_django/FridgeoBack/fridgeo/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('', views.index, name='startpage'),
     path('secondpage/', views.HelloThere, name='seconpage'),
     path('post/<int:post_id>/', views.show_post, name='post'),
-    path('/information', views.information, name='information'),
-    path('/login', views.login, name='login'),
-    path('/account', views.account, name='account')
+    path('information/', views.information, name='information'),
+    path('login/', views.login, name='login'),
+    path('account/', views.account, name='account')
 ]
